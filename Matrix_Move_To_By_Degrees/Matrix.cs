82cb073f3c848c8cacc8ff7ac21063d07f4bc0ea
Yuriy Parashynets <yura.parashynets@gmail.com>
2021-09-13 15:17:08 +0300

    master - small update.

--- Matrix_Move_To_By_Degrees/Matrix.cs
+++ Matrix_Move_To_By_Degrees/Matrix.cs
@@ -7,15 +7,6 @@ namespace Matrix_Move_To_By_Degrees
 {
     public class Matrix
     {
-        public readonly int[][] arr =
-        {
-            new int [] { 1, 2, 3, 4, 7 },
-            new int [] { 4, 3, 2, 5, 8 },
-            new int [] { 5, 2, 1, 5, 4 },
-            new int [] { 6, 5, 3, 1, 9 },
-            new int [] { 2, 6, 1, 8, 3 },
-        };
-
         public int[][] MoveByDegrees(int[][] arr)
         {
             int[][] copiedArr = new int[arr.Length][];
